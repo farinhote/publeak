0c06e5efd3d97edc6febcce73d23fd03dcd6e5b0
Farinhote <andrefarinhote@gmail.com>
2020-01-26 19:30:04 +0000

    Removed test CSS and Class

--- src/components/AboutText.vue
+++ src/components/AboutText.vue
@@ -4,7 +4,6 @@
     <p>{{ $t("about.tech") }}</p>
     <a
       href="https://github.com/farinhote/"
-      class="brk-btn"
     >Farinhote</a>
   </div>
 </template>
@@ -81,48 +80,4 @@ a:after {
   border-color: rgba(0, 0, 0, 0) rgba(0, 0, 0, 0) #9B541E #9B541E;
   box-shadow: 25px 25px 25px rgba(46, 46, 49, .8);
 }
-
-/* .brk-btn {
-  position: relative;
-  background: none;
-  color: aqua;
-  text-transform: uppercase;
-  text-decoration: none;
-  border: 0.2em solid aqua;
-  padding: 0.5em 1em;
-}
-
-.brk-btn::before {
-  content: "";
-  display: block;
-  position: absolute;
-  width: 10%;
-  background: #222;
-  height: 0.3em;
-  right: 20%;
-  top: -0.21em;
-  transform: skewX(-45deg);
-  -webkit-transition: all 0.45s cubic-bezier(0.86, 0, 0.07, 1);
-  transition: all 0.45s cubic-bezier(0.86, 0, 0.07, 1);
-}
-.brk-btn::after {
-  content: "";
-  display: block;
-  position: absolute;
-  width: 10%;
-  background: #222;
-  height: 0.3em;
-  left: 20%;
-  bottom: -0.25em;
-  transform: skewX(45deg);
-  -webkit-transition: all 0.45 cubic-bezier(0.86, 0, 0.07, 1);
-  transition: all 0.45s cubic-bezier(0.86, 0, 0.07, 1);
-}
-.brk-btn:hover::before {
-  right: 80%;
-}
-.brk-btn:hover::after {
-  left: 80%;
-} */
-
 </style>
